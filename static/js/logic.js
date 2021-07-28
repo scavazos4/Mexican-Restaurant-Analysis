baed445b449a0b9b24eccbea454b0c5e9c9fcc45
Shawn Cavazos <scavazos4@gmail.com>
2021-07-28 11:50:07 -0500

    d

--- static/js/logic.js
+++ static/js/logic.js
@@ -27,30 +27,20 @@ L.geoJson(statesData).addTo(myMap);
 
 
 function getColor(d) {
-  return d > 0.50  ? '#800026' :
-         d > 0.45  ? '#BD0026' :
-         d > 0.40  ? '#E31A1C' :
-         d > 0.35  ? '#FC4E2A' :
-         d > 0.30  ? '#FD8D3C' :
-         d > 0.25  ? '#FEB24C' :
-         d > 0.20  ? '#FED976' :
+  return d > 0.35  ? '#800026' :
+         d > 0.30  ? '#BD0026' :
+         d > 0.25  ? '#E31A1C' :
+         d > 0.20  ? '#FC4E2A' :
+         d > 0.15  ? '#FD8D3C' :
+         d > 0.10  ? '#FEB24C' :
+         d > 0.05  ? '#FED976' :
                    '#FFEDA0';
 }
 
-d3.csv("/data/hispanicData.csv")
-  .row(function style(data) {
-    return {
-      Percent_Hispanic: data.Percent_Hispanic
-  };
-})
-.get(function(data) {
-  console.log(data);
-});
 
-
-function style(d) {
+function style(feature) {
   return {
-      fillColor: getColor(d.Percent_Hispanic),
+      fillColor: getColor(feature.properties.hisp),
       weight: 2,
       opacity: 1,
       color: 'white',
@@ -60,4 +50,5 @@ function style(d) {
 }
 
 L.geoJson(statesData, {style: style}).addTo(myMap);
+
 
