9ed55b90e91ff1951ae3f051c30dbb01058483f2
Shawn Cavazos <scavazos4@gmail.com>
2021-07-28 14:04:21 -0500

    note

--- static/js/logic.js
+++ static/js/logic.js
@@ -25,16 +25,18 @@ var myMap = L.map("map", {
 
 L.geoJson(statesData).addTo(myMap);
 
+var geojson;
 
 function getColor(d) {
-  return d > 0.35  ? '#800026' :
-         d > 0.30  ? '#BD0026' :
-         d > 0.25  ? '#E31A1C' :
-         d > 0.20  ? '#FC4E2A' :
-         d > 0.15  ? '#FD8D3C' :
-         d > 0.10  ? '#FEB24C' :
-         d > 0.05  ? '#FED976' :
-                   '#FFEDA0';
+  return d > 0.40  ? '#7f0000' :
+         d > 0.35  ? '#b30000' :
+         d > 0.30  ? '#d7301f' :
+         d > 0.25  ? '#ef6548' :
+         d > 0.20  ? '#fc8d59' :
+         d > 0.15  ? '#fdbb84' :
+         d > 0.10  ? '#fdd49e' :
+         d > 0.05  ? '#fee8c8' :
+                     '#fff7ec';
 }
 
 
@@ -51,4 +53,80 @@ function style(feature) {
 
 L.geoJson(statesData, {style: style}).addTo(myMap);
 
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+  }
+  info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+
+
+function onEachFeature(feature, layer) {
+  layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+  });
+}
+
+
+geojson = L.geoJson(statesData, {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(myMap);
+
+
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Hispanic Population Percentage</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.hisp + ' %'
+        : 'Hover over a state');
+};
+
+info.addTo(myMap);
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.35, 0.40],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
 
