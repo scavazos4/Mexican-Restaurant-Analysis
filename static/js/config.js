e5ef429309261f6b0280148c45881f0307f292bd
Shawn Cavazos <scavazos4@gmail.com>
2021-07-30 11:25:24 -0500

    note

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 const APILeaflet = "pk.eyJ1Ijoic2NhdmF6b3M0IiwiYSI6ImNrcjQzOXZreTFuaWwyb3J4ajcxaG5xN3MifQ.5AMTV-NKX21kbJ8fzwGLow";
 const APICensus = "64f1dfb2f21e446d237909b1045fcde98e66d4f8"
+const Yelp= "PnCaCClu_oaMrigxA4QIQILSm_-IZtvKAJur803egyHClYLF1A93_cEYvgBY8Na2fpa65VSTjDtofak2coDyz3iUCQHuhL6Y3rRN6mnfIBzRhgKLu5bwmzq6A_8CYXYx"
